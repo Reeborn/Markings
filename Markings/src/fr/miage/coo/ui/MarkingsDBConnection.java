1f69eb1d9ea1df9ec91c66cf8df832d95808bef3
Ah Hen David <ahhen@b13p20.fil.univ-lille1.fr>
2014-12-04 17:12:34 +0100

    [DAN] DB connection

--- Markings/src/fr/miage/coo/ui/MarkingsDBConnection.java
+++ Markings/src/fr/miage/coo/ui/MarkingsDBConnection.java
@@ -2,6 +2,8 @@ package fr.miage.coo.ui;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class MarkingsDBConnection {
@@ -9,7 +11,13 @@ public class MarkingsDBConnection {
 	public static Connection connexion;
 
 	public static void main(String[] args) throws SQLException {
-		connexion = DriverManager.getConnection("jdbc:hsqlbd:file:bdd/Markings;shutdown=true", "user", "user");
+		connexion = DriverManager.getConnection("jdbc:hsqldb:file:bdd/Markings;shutdown=true", "user", "user");
+		PreparedStatement st = connexion.prepareStatement("select * from Student");
+		ResultSet rs = st.executeQuery();
+		while (rs.next()) {
+			System.out.println(rs.getString(2));
+		}
+		connexion.close();
 	}
 
 }
